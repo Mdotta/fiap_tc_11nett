1d49ba33afd970bb013b513168c18f8c8a5c947f
Mdotta <md.correa@outlook.com>
2025-12-24 00:02:42 -0300

    fix:ajustes no projeto para possibilitar deployment via docker-compose

--- Postech.NETT11.PhaseOne/Postech.NETT11.PhaseOne.Infrastructure/Repository/Configurations/UserConfiguration.cs
+++ Postech.NETT11.PhaseOne/Postech.NETT11.PhaseOne.Infrastructure/Repository/Configurations/UserConfiguration.cs
@@ -47,7 +47,7 @@ public class UserConfiguration:IEntityTypeConfiguration<User>
     {
         var userAdmin = new User()
         {
-            Id = Guid.NewGuid(),
+            Id =  new Guid("6C511C9F-CAE7-4D08-8101-F8AF7C81357A"),
             CreatedAt = new DateTime(year:2025, month:1, day:1),
             UserHandle = "admin",
             Username = "admin",
@@ -57,7 +57,7 @@ public class UserConfiguration:IEntityTypeConfiguration<User>
         
         var userClient = new User()
         {
-            Id = Guid.NewGuid(),
+            Id = new Guid("4D9C6BD6-821A-40F0-B2B9-64683B5E91E1"),
             CreatedAt = new DateTime(year:2025, month:1, day:1),
             UserHandle = "client",
             Username = "client",
